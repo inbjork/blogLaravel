ed308adb88791dee820f2f5690d6caa771d9e032
inbjork <emilioabautista@gmail.com>
2018-11-02 13:07:05 -0600

    enablePostsDashboard

---
+++ resources/views/admin/partials/nav.blade.php
@@ -0,0 +1,19 @@
+<ul class="sidebar-menu" data-widget="tree">
+    <li class="header">Navegacion</li>
+
+    <!-- Optionally, you can add icons to the links -->
+
+    <li class="active"><a href="#"><i class="fa fa-dashboard"></i> <span>Inicio</span></a></li>
+    {{-- <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li> --}}
+    <li class="treeview">
+    <a href="#"><i class="fa fa-bars"></i> <span>Blog</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+    </a>
+    <ul class="treeview-menu">
+        <li><a href="#"><i class="fa fa-eye"></i> Ver todos los Posts</a></li>
+        <li><a href="#"><i class="fa fa-pencil"></i> Crear un Post</a></li>
+    </ul>
+    </li>
+</ul>
